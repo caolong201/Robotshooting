58030c47d72a93a8e0528278a95fd0ec60b62b01
caolong201 <126745101+caolong201@users.noreply.github.com>
2025-06-16 16:23:25 +0700

    upClassEnemy

--- Assets/Scripts/GamePlay/Enemy/Gun.cs
+++ Assets/Scripts/GamePlay/Enemy/Gun.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Gun : MonoBehaviour
 {
@@ -11,10 +12,22 @@ public class Gun : MonoBehaviour
     public int bulletsPerBurst =10;
     public float timeBetweenBullets = 0.05f;
     public float timeBetweenBursts = 5f;
-    //public Transform target;
+    public Transform target;
+    public float hitAccuracy = 0.5f; // Tỷ lệ bắn trúng (50%)
+    public float missAngleRange = 20f; // Độ lệch khi bắn tự do
+    public float rotationSpeed = 0.5f; // Tốc độ xoay súng (xoay mượt)
+    private Vector3 currentShootDirection;
     private void Start()
     {
         StartCoroutine(AutoFire());
+    }
+    private void Update()
+    {    
+        if (currentShootDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(currentShootDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
     private IEnumerator AutoFire()
     {
@@ -31,13 +44,26 @@ public class Gun : MonoBehaviour
     }
     public void Shoot()
     {
-        if (bulletPrefab == null || firePoint == null)
+        if (bulletPrefab == null || firePoint == null || target ==null)
         {
             Debug.LogWarning("ll");
             return;
         }
-      
-
+        Vector3 directionToTarget = (target.position - firePoint.position).normalized;
+        Vector3 shootDirection;
+    
+        //firePoint.rotation = Quaternion.LookRotation(directionToTarget);
+        if (Random.value <= hitAccuracy)
+        {
+            // Bắn trúng
+            shootDirection = directionToTarget;
+        }
+        else
+        {
+            // Bắn tự do
+            shootDirection = GetRandomDirection(directionToTarget, missAngleRange);
+        }
+        currentShootDirection = shootDirection;
         GameObject bulletObj = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Debug.Log("Spawned bullet: " + bulletObj.name);
         Bullet bullet = bulletObj.GetComponent<Bullet>();
@@ -49,5 +75,12 @@ public class Gun : MonoBehaviour
         }
         Debug.DrawRay(firePoint.position, firePoint.forward * bulletRange, Color.red, 1f);
     }
+    private Vector3 GetRandomDirection(Vector3 baseDirection, float maxAngle)
+    {
+        float randomAngleX = Random.Range(-maxAngle, maxAngle);
+        float randomAngleY = Random.Range(-maxAngle, maxAngle);
 
+        Quaternion randomRotation = Quaternion.Euler(randomAngleX, randomAngleY, 0);
+        return randomRotation * baseDirection;
+    }
 }
