be6a70ab3054a54a73d997c497ec1514aed799ad
TuongCao <84375320+TuongCao-crypto@users.noreply.github.com>
2025-06-19 11:37:57 +0700

    remove old asset

--- Assets/AssetReference/MechCombatKit - Main/VehicleCombatKits/Scripts/Systems/GameStateSystem/GameState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace VSX.GameStates
-{
-    [CreateAssetMenu(menuName = "VSX/Game State")]
-    public class GameState : ScriptableObject { }
-}
