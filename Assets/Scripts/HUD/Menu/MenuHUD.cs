36cc26b4de2650b99dc0e243a70bef8121decd82
caolong201 <126745101+caolong201@users.noreply.github.com>
2025-06-23 15:00:18 +0700

    UpdateMenu

--- Assets/Scripts/HUD/Menu/MenuHUD.cs
+++ Assets/Scripts/HUD/Menu/MenuHUD.cs
@@ -1,28 +1,38 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MenuHUD : MonoBehaviour
 {
     [SerializeField] List<MenuMapItem> menuItems = new List<MenuMapItem>();
-
+    public RectTransform textTransform;
+    public RectTransform buttonTransform;
     private int currentStageSelected = 1;
-
-    // Start is called before the first frame update
+    public float minScale = 0.8f; 
+    public float maxScale = 1.2f; 
+    public float pulseDuration = 1f; 
+    private Tween pulseTween;
     void Start()
     {
+        currentStageSelected = PlayerPrefs.GetInt("kCurrentStage", 1);
         // init UI
         for (int i = 0; i < menuItems.Count; i++)
         {
             menuItems[i].Init(this, i + 1);
-        }
-
+        }  
+        OnSelectedStage(currentStageSelected);
         //ScreenFader.Instance.FadeIn(0);
+        StartPulse();
     }
-
     public void OnSelectedStage(int stage)
     {
+        currentStageSelected = stage;
+        PlayerPrefs.SetInt("kCurrentStage", currentStageSelected);
+        PlayerPrefs.Save();
+
         currentStageSelected = stage;
         foreach (var item in menuItems)
         {
@@ -36,15 +46,36 @@ public class MenuHUD : MonoBehaviour
             }
         }
     }
-
     public void OnbtnPlayClicked()
-    {
-
-
+    {    
         PlayerPrefs.SetInt("kCurrentStage", currentStageSelected);
-        PlayerPrefs.SetInt("kCurrentWave", 1); 
-
+        PlayerPrefs.SetInt("kCurrentWave", 1);
         ScreenFader.Instance.LoadScene(1);
     }
+    public void StartPulse()
+    {
+        buttonTransform.localScale = Vector3.one;
+        pulseTween = buttonTransform
+            .DOScale(minScale, pulseDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
 
+    }
+    public void StopPulse()
+    {
+        if (pulseTween != null)
+        {
+            pulseTween.Kill();
+            buttonTransform.localScale = Vector3.one;
+        }
+    }
 }
+
+
+    
+
+
+  
+       
+   
+
