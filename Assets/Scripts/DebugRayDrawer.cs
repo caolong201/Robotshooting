e646484108c6d564199f80849e722089179b1a53
TuongCao <84375320+TuongCao-crypto@users.noreply.github.com>
2025-06-27 09:29:13 +0700

    Create DebugRayDrawer.cs

---
+++ Assets/Scripts/DebugRayDrawer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugRayDrawer : MonoBehaviour
+{
+    public Transform target;
+    void OnDrawGizmos()
+    {
+        if (target != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+    }
+}
